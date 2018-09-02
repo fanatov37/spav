b097166297903cbda6dae3155556dda4cccf595b
Fanatov <fanatov@Fanatovs-MacBook-Pro.local>
2018-09-02 14:58:50 +0300

    refactoring ServiceMenager for token

--- src/ServiceManager/ServiceManager.php
+++ src/ServiceManager/ServiceManager.php
@@ -1,22 +1,25 @@
 <?php
+namespace Spav\ServiceManager;
+
+use Interop\Container\ContainerInterface;
+use Zend\Authentication\AuthenticationService;
+use Zend\Authentication\Storage\Session;
+use Zend\Db\Adapter\Adapter;
+use Zend\I18n\Translator\Translator;
+use Zend\Log\Logger;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\Session\Container;
+use Zend\Session\Storage\ArrayStorage;
 /**
- * ServiceManager
+ * Class ServiceManager
  *
  * @link https://github.com/fanatov37/spav.git for the canonical source repository
  * @copyright Copyright (c) 2015
  * @license YouFold (c)
  * @author VladFanatov
- * @package Library
+ *
+ * @package Spav\ServiceManager
  */
-namespace Spav\ServiceManager;
-
-use Interop\Container\ContainerInterface;
-use Zend\Authentication\AuthenticationService;
-use Zend\I18n\Translator\Translator;
-use Zend\Log\Logger;
-use Zend\ServiceManager\ServiceLocatorInterface;
-
-
 abstract class ServiceManager
 {
     /**
@@ -43,7 +46,6 @@ abstract class ServiceManager
     {
         return $this->sm;
     }
-
     /**
      * @param $name
      *
@@ -60,7 +62,6 @@ abstract class ServiceManager
 
         return $this->sm->get($name);
     }
-
     /**
      * @return Translator
      * @throws \Exception
@@ -71,7 +72,6 @@ abstract class ServiceManager
     {
         return $this->getService('translator');
     }
-
     /**
      * @return array
      * @throws \Exception
@@ -82,7 +82,6 @@ abstract class ServiceManager
     {
         return $this->getService('config');
     }
-
     /**
      * @return Logger
      * @throws \Exception
@@ -93,7 +92,6 @@ abstract class ServiceManager
     {
         return $this->getService('log');
     }
-
     /**
      * @return array|null
      * @throws \Exception
@@ -106,5 +104,44 @@ abstract class ServiceManager
         $authService = $this->getService(AuthenticationService::class);
 
         return $authService->getIdentity();
+    }
+    /**
+     * @param string $token
+     *
+     * @return int|null
+     */
+    public function getUserDataByToken(string $token) : ?array
+    {
+        /** @var Adapter $adapter */
+        $adapter = $this->sm->get(Adapter::class);
+        $query = $adapter->query("
+            select
+              user_id,
+              language_id
+            from oauth_access_tokens
+            inner join user on user.id=oauth_access_tokens.user_id
+            where access_token = '{$token}'
+        ");
+        $result = $query->execute();
+
+        return $result->current();
+    }
+    /**
+     * @param string $token
+     *
+     * @throws \Exception
+     */
+    public function initIdentityByToken(string $token)
+    {
+        $userData = $this->getUserDataByToken($token);
+        $sessionStorage = new Session();
+        $sessionStorage->write([
+            'userId' => (int)$userData['user_id'],
+            'languageId' => (int)$userData['language_id']
+        ]);
+
+        /** @var AuthenticationService $authService */
+        $authService = $this->getService(AuthenticationService::class);
+        $authService->setStorage($sessionStorage);
     }
 }
